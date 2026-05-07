2d0d5d8a1603161ff2213cd4460d5ca8e4f49881
Xamacardoso <contato.xcardoso@gmail.com>
2026-05-07 11:21:14 -0300

    :truck: Chore (Build): Modificando as dependencias e versoes de apps do docker para rodar com o ambiente similar ao intel.

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,6 +83,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "task_manager.users",
+    "task_manager.tasks",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
